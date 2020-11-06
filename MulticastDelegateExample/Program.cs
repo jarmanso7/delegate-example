cca02d9a456cd84d2f53aba4844973e4892cea82
jarmanso7 <jordi.armangue.soler@hotmail.com>
2020-11-07 00:46:09 +0100

    invoking multicast delegate with several targets

--- MulticastDelegateExample/Program.cs
+++ MulticastDelegateExample/Program.cs
@@ -4,31 +4,57 @@ namespace MulticastDelegateExample
 {
     class Program
     {
+        delegate int Del(int a, int b);
+
         static void Main(string[] args)
         {
             var mark = new Mark();
 
-            mark.AddNumbers(3, 3);
-            mark.MultiplyNumbers(3, 3);
-            mark.SubstractNumbers(3, 3);
+            Del multicastDelegate;
+
+            multicastDelegate = mark.AddNumbers;
+            Console.WriteLine("Invoking delegate with one target:");
+            multicastDelegate(6, 5);
+            Console.WriteLine();
+
+            multicastDelegate += mark.MultiplyNumbers;
+            Console.WriteLine("Invoking delegate with two targets:");
+            multicastDelegate(6, 5);
+            Console.WriteLine();
+
+            multicastDelegate += mark.SubstractNumbers;
+            Console.WriteLine("Invoking delegate with three targets:");
+            multicastDelegate(6, 5);
+            Console.WriteLine();
+
+            multicastDelegate -= mark.MultiplyNumbers;
+            Console.WriteLine("Invoking delegate without MultiplyNumbers:");
+            multicastDelegate(6, 5);
+            Console.WriteLine();
         }
     }
 
     public class Mark
     {
-        public void AddNumbers(int a, int b)
+        public int AddNumbers(int a, int b)
         {
-            Console.WriteLine($"{nameof(AddNumbers)} : a + b = {a + b}");
+            var sum = a + b;
+            Console.WriteLine($"{nameof(AddNumbers)} : a + b = {sum}");
+            return sum;
         }
 
-        public void MultiplyNumbers(int a, int b)
+        public int MultiplyNumbers(int a, int b)
         {
-            Console.WriteLine($"{nameof(AddNumbers)} : a * b = {a * b}");
+            var product = a * b;
+            Console.WriteLine($"{nameof(MultiplyNumbers)} : a * b = {product}");
+            return product;
         }
 
-        public void SubstractNumbers(int a, int b)
+        public int SubstractNumbers(int a, int b)
         {
-            Console.WriteLine($"{nameof(AddNumbers)} : a - b = {a - b}");
+            var substraction = a - b;
+            Console.WriteLine($"{nameof(SubstractNumbers)} : a - b = {substraction}");
+            return substraction;
         }
     }
 }
